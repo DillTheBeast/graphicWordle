392947fd588eb0a786557b82547e2a1243580efd
DillTheBeast <dillonmaltese@gmail.com>
2022-07-27 14:01:24 -0400

    g

--- src/Wordle/Controller.java
+++ src/Wordle/Controller.java
@@ -1,12 +1,20 @@
 package Wordle;
 
+import javax.swing.plaf.multi.MultiMenuBarUI;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
 public class Controller {
 
@@ -34,22 +42,69 @@ public class Controller {
     @FXML
     private Button enterButton;
 
-    Alert multiNumbers;
+    Alert multiLetters;
+    Alert number;
+    String secretWord = "ratio";
+    Background backFillGreen;
+    Background backFillYellow;
+    GridPane[] grid;
+    int idx = 0;
 
     public void initialize() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
+        
+        grid[0] = grid1;
+        grid[1] = grid2;
+        grid[2] = grid3;
+        grid[3] = grid4;
+        grid[4] = grid5;
+        grid[5] = grid6;  
+    }
+
+    public Controller() {
+        multiLetters = new Alert(AlertType.ERROR);
+        multiLetters.setHeaderText("Invalid letter amount.");
+        multiLetters.setContentText("There should be one letter in each box.");
+        number = new Alert(AlertType.ERROR);
+        number.setHeaderText("Invalid input");
+        number.setContentText("There should only be a letter in the box. No numbers or characters.");
+        backFillGreen = new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY));
+        backFillYellow = new Background(new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY));
+        //Making gridpane array = to something
+        grid = new GridPane[6];
     }
     @FXML
     void onEnterClick(ActionEvent event) {
-        try {
-            grid1.getChildren();
-        }
-        catch(Exception e) {
-        multiNumbers = new Alert(AlertType.ERROR);
-        multiNumbers.setHeaderText("Multiple Numbers");
-        multiNumbers.setContentText("You can only have one number in each box.");
-        }
-    }
-     
+            for(int i = 0; i < 5; i++) {
+                TextField currentField = ((TextField)grid[idx].getChildren().get(i));
+                String currentLetter = currentField.getText();
+                
+                if(currentLetter.length() != 1) {
+                    multiLetters.showAndWait();
+                    return;
+                }
+                if(currentLetter.charAt(0) > 65 & currentLetter.charAt(0) < 90||currentLetter.charAt(0) > 97 & currentLetter.charAt(0) < 122) {
+                    if(currentLetter.charAt(0) == secretWord.charAt(i)) {
+                        System.out.println(currentLetter + " is in the right spot");
+                        currentField.setBackground(backFillGreen);
+                    }
+                    //If currentLetter is somewhere inside of secretWord
+                    else if(secretWord.contains(currentLetter)){
+                        System.out.println(currentLetter + " is in the word but placed in the wrong spot");
+                        currentField.setBackground(backFillYellow); 
+                    }
+                    else {
+                        System.out.println(currentLetter + " is not in the word");
+                    }
+                }
+                else {
+                    number.showAndWait();
+                    return;
+                }
+                idx++;
+                
+            }
+        
+    } 
 }
