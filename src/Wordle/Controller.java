556fb693136844d4a644975ca0c7b022fe8c6826
DillTheBeast <dillonmaltese@gmail.com>
2022-07-27 10:43:16 -0400

    g

--- src/Wordle/Controller.java
+++ src/Wordle/Controller.java
@@ -1,7 +1,11 @@
 package Wordle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 
 public class Controller {
@@ -27,10 +31,25 @@ public class Controller {
     @FXML
     private GridPane grid6;
 
+    @FXML
+    private Button enterButton;
+
+    Alert multiNumbers;
+
     public void initialize() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+    }
+    @FXML
+    void onEnterClick(ActionEvent event) {
+        try {
+            grid1.getChildren();
+        }
+        catch(Exception e) {
+        multiNumbers = new Alert(AlertType.ERROR);
+        multiNumbers.setHeaderText("Multiple Numbers");
+        multiNumbers.setContentText("You can only have one number in each box.");
+        }
     }
      
 }
--- src/Wordle/Main.java
+++ src/Wordle/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("wordle.fxml"));
         primaryStage.setTitle("Wordle");
-        primaryStage.setScene(new Scene(root, 600, 652));
+        primaryStage.setScene(new Scene(root, 600, 700));
         primaryStage.show();
     }
 
