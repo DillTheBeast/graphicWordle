0f0c8407e4dbfb290c41abec89074edf03ed2926
DillTheBeast <dillonmaltese@gmail.com>
2022-07-29 10:32:31 -0400

    t

--- src/Wordle/Main.java
+++ src/Wordle/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("wordle.fxml"));
         primaryStage.setTitle("Wordle");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("wordleicon.png")));
         Scene scene = new Scene(root, 600, 750);
         scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         primaryStage.setScene(scene);
